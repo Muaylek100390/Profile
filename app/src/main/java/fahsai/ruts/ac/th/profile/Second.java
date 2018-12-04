72068cb06eca7f253935509122fa4b80930e55c4
a10305-5 <123>
2018-12-04 12:22:33 +0700

    introduce add button putextra

--- app/src/main/java/fahsai/ruts/ac/th/profile/Second.java
+++ app/src/main/java/fahsai/ruts/ac/th/profile/Second.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class Second extends AppCompatActivity {
 
     ImageButton imgprev ,imgnext;
+    EditText username,password,tel;
+
 
 
 
@@ -17,6 +20,9 @@ public class Second extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        username = (EditText)findViewById(R.id.ID);
+        password = (EditText)findViewById(R.id.Password);
+        tel = (EditText)findViewById(R.id.Tel);
 
         imgprev = (ImageButton)findViewById(R.id.Prev);
         imgprev.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +37,11 @@ public class Second extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Second.this,Third.class);
+
+                intent.putExtra("name",username.getText().toString());
+                intent.putExtra("password",password.getText().toString());
+                intent.putExtra("phone",tel.getText().toString());
+
                 startActivity(intent);
             }
         });
--- app/src/main/java/fahsai/ruts/ac/th/profile/Third.java
+++ app/src/main/java/fahsai/ruts/ac/th/profile/Third.java
@@ -2,12 +2,17 @@ package fahsai.ruts.ac.th.profile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Third extends AppCompatActivity {
+
+    TextView username,password,tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        
     }
 }
